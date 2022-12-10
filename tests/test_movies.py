0c7f17384dd84384b1908598d1e268431c282a18
Vishag K <32572363+MagmaCode55@users.noreply.github.com>
2022-12-10 13:31:25 -0500

    Not breaking definitely

--- tests/test_movies.py
+++ tests/test_movies.py
@@ -15,7 +15,7 @@ def test_get_all_movies(test_app: FlaskClient):
 
     assert res.status_code == 200
     assert f'<td><a href="/movies/{test_movie.movie_id}">The Dark Knight</a></td>' in page_data
-    assert '<td>Christopher Nolan</td>' in page_data
+    assert '<td>Christopher Nolan1</td>' in page_data
     assert '<td>5</td>' in page_data
 
 def test_get_all_movies_empty(test_app: FlaskClient):
